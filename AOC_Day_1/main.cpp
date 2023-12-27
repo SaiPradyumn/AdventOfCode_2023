0e9d53c46d89f79439252aeb4bb8a1d71b6ccecc
SaiPradyumn <shrivastavasp9@gmail.com>
2023-12-26 20:12:56 -0500

    added folder

--- AOC_Day_1/main.cpp
+++ AOC_Day_1/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
 #include <string>
 
 using namespace std;
+
+
+
+int process_line_partII(string line){
+
+}
 
 int process_line(string line){
     int dig1 = -1;
@@ -13,27 +18,21 @@ int process_line(string line){
             if(dig1 == -1){
                 char st = line[i];
                 dig1 = st-'0';
-                //cout<<dig1;
             }
                 char st = line[i];
                 dig2 = st-'0';
-
         }
     }
-//    if(dig1 == -1 || dig2 == -1){
-//        return 0;
-//    }
-
     return dig1*10 + dig2;
 }
 
 
 int main() {
     fstream fin;
-    fin.open("../input_1.txt", ios::in);
+    fin.open("../AOC_Day_1/input_1.txt", ios::in);
     int answer = 0;
     if(fin.is_open()){
-        cout<<"file open";
+        //cout<<"file open";
         string line;
         int line_sum = 0;
         while(getline(fin,line)){
